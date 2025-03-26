58fa80aa17555af6012ae3585db1082c30762bf7
Melanie Roberts <robertsmelaniecc@gmail.com>
2025-03-26 11:42:16 -0400

    about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,6 +4,37 @@ function About() {
   return (
     <div>
       <h2>About the Pet Store</h2>
+      <p>
+                                At The Mighty Mouser when it comes to rodent control we believe in working smarter not harder. Farmers have known for generations that barn cats are the best defense against mice, rats, and other pests. A tried-and-true partnership between cat and farm.
+                            </p>  
+                            <p>
+                                Our mission is simple help get the right supplies for: 
+                                <ul>
+                                    <li>Farmers, </li>
+                                    <li>
+                                        Ranchers,
+                                    </li>
+                                    <li>
+                                        and Homesteaders
+                                    </li>
+                                                                        
+                                </ul>
+                                 <br>
+                                
+                                <!-- <p id="font1" font="Arial">
+                                <font="Arial">test</font>These cats aren’t just pets; they’re hardworking, independent, and an essential part of a well-run operation. Give them shelter, a little food, and they’ll work hard for you protecting your feed, equipment, and crops like they’ve done for centuries
+                            </p> -->
+                            <p>
+                                At The Mighty Mouser, we provide everything you need to support your barn cats including:
+                                <ul>
+                                    <li>Quality feed</li>
+                                    <li>Sturdy shelters</li>
+                                    <li>Other suppliles to keep them ready for the job</li>
+                                </ul>
+                            </p>
+                            <p>
+                                Whether you’ve been farming for decades or are just getting started, we’re here to help you build a sustainable, rodent-free farm one barn cat at a time.
+                            </p>
       <p>The Sun is so large that about 1.3 million Earths could fit inside it.</p>
     </div>
   );
