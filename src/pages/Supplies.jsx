2576d073f92f0eea4d6963f3f254c37ea370da46
Melanie Roberts <robertsmelaniecc@gmail.com>
2025-04-03 16:49:20 -0400

    update

--- src/pages/Supplies.jsx
+++ src/pages/Supplies.jsx
@@ -3,42 +3,44 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import '../styles/Components.css';
 import FooterButtons from '../components/Footerbutton';
+import BarnCatsImage from '../components/BarnCatsImage';
 
 
 function Supplies() {
     return (
         <>
-            
-            <main className="main">
-                <section className="hero">
+
+            <main>
+                <section>
                     <h2>Welcome to Our Pet Store!</h2>
                     <p>Your one-stop shop for all things pet-related.</p>
+                    <div className="title">
+                        <h1>
+                            Supplies
+                        </h1>
+                    </div>
+                    <div className="container">
+                        <div className="container1">
+                            <div className="containertext">
+                                <div className="supply-container">
+                                    <h3><em>Our Supplies</em></h3>
+                                    <barncatsimage>
+                                        <br />
+                                        <div id="market" style={{ textAlign: 'center' }}>
+                                        </div>
+                                    </barncatsimage></div>
+                                <br />
+                                <div id="market" />
+                            </div>
+                        </div>
+                    </div>
                 </section>
-<div>
-  <div className="title">
-    <h1>
-      Supplies
-    </h1>
-  </div>
-  <div className="container">
-    <div className="container1">
-      <div className="containertext">
-        <div className="supply-container">
-          <h3><em>Our Supplies</em></h3>
-          {/* <img src="illustration_for_barn_cats_9.png" alt="BarnCats" width="250px"> */}
-          <br />
-          <div id="market" style={{textAlign: 'center'}} />
-        </div>
-        <br />
-        <div id="market" />
-      </div>
-    </div></div></div>
-
             </main>
-            
-            
+
+
         </>
     );
-}
+};
+
 
 export default Supplies;
