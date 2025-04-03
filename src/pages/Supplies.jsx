7076110d29db0839a3553b3476f5f4c33594055e
Melanie Roberts <robertsmelaniecc@gmail.com>
2025-04-03 04:26:33 -0400

    supplies

--- src/pages/Supplies.jsx
+++ src/pages/Supplies.jsx
@@ -14,6 +14,26 @@ function Supplies() {
                     <h2>Welcome to Our Pet Store!</h2>
                     <p>Your one-stop shop for all things pet-related.</p>
                 </section>
+<div>
+  <div className="title">
+    <h1>
+      Supplies
+    </h1>
+  </div>
+  <div className="container">
+    <div className="container1">
+      <div className="containertext">
+        <div className="supply-container">
+          <h3><em>Our Supplies</em></h3>
+          {/* <img src="illustration_for_barn_cats_9.png" alt="BarnCats" width="250px"> */}
+          <br />
+          <div id="market" style={{textAlign: 'center'}} />
+        </div>
+        <br />
+        <div id="market" />
+      </div>
+    </div></div></div>
+
             </main>
             
             
