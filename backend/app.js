044508c35f485246a3434f064863a6ea7e16140b
Melanie Roberts <robertsmelaniecc@gmail.com>
2025-04-05 17:24:59 -0400

    can't find a second port

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
@@ -12,11 +13,16 @@ app.use(express.static('public'));
 
 // app.set('view engine', 'ejs');  I don't think I need this one
 
+
+console.log('🔍 MONGO_URI =', JSON.stringify(process.env.MONGO_URI));
 //database connection
-const dbURI = 'mongodb.srv://more'
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
-  .then((result) => app.listen(3001))
+const dbURI = process.env.MONGO_URI; 
+console.log('RAW URI:', [...process.env.MONGO_URI]);
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3009))
   .catch((err) => console.log(err));
+
+
 
   //routes
 app.get('/', (req, res) => res.render('home'));
@@ -24,7 +30,7 @@ app.get('/items', (req, res) => res.render('items'));
 
 app.use(productRoutes);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3009;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
