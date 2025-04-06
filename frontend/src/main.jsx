6801a214bbe0bd31e98183b861dab6eb0bc6d2e7
Melanie Roberts <robertsmelaniecc@gmail.com>
2025-04-05 22:34:51 -0400

    move to frontend folder

---
+++ frontend/src/main.jsx
@@ -0,0 +1,31 @@
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// // import "./styles/style.css";
+// import App from './App.jsx';
+// import '../styles/Components.css';
+// import { BrowserRouter } from 'react-router-dom';
+// im
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <BrowserRouter>
+
+//       <App />
+
+//     </BrowserRouter>
+    
+//   </StrictMode>
+// );
+// File: /src/main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './styles/style.css'; // global styles
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    
+    <App /> 
+    
+  </React.StrictMode>
+);
