a37bc173f7623c6b738af0c105af6f54e1b43982
Melanie Roberts <robertsmelaniecc@gmail.com>
2025-04-06 12:17:49 -0400

    reformat interrupted

--- frontend/src/pages/Supplies.jsx
+++ frontend/src/pages/Supplies.jsx
@@ -15,13 +15,36 @@ function Supplies() {
     return (
         <>
             <main>
-                <section className="text-center my-4">
+                {/* <section className="text-center my-4">
                     <h2>Welcome to Our Pet Store!</h2>
                     <p>Your one-stop shop for all things pet-related.</p>
                     <h1 className="my-4">Supplies</h1>
-                    {/* <BarnCatsImage /> */}
-                </section>
-
+                    {/* <BarnCatsImage /> 
+                </section> */}
+                <section>
+                                         <h2>Welcome to Our Pet Store!</h2>
+                                         <p>Your one-stop shop for all things pet-related.</p>
+                                         <div className="title">
+                                             <h1>
+                                                 Supplies
+                                             </h1>
+                                         </div> 
+                                         <div className="container">
+                                             <div className="container1">
+                                                 <div className="containertext">
+                                                     <div className="supply-container">
+                                                         <h3><em>Our Supplies</em></h3>
+                                                         <barncatsimage>
+                                                             <br />
+                                                             <div id="market" style={{ textAlign: 'center' }}>
+                                                             </div>
+                                                         </barncatsimage></div>
+                                                     <br />
+                                                     <div id="market" />
+                                                 </div>
+                                             </div>
+                                         </div>
+                                     </section>
                 <section className="container">
                     <div className="row">
                         {products.map((product, index) => (
