7baceae82262ab259fa9cf838fbfe885368bae07
Melanie Roberts <robertsmelaniecc@gmail.com>
2025-04-07 08:54:58 -0400

    update viteconfig

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Specify your desired output directory here
+    emptyOutDir: true, // Optional: Clears the output directory before building
+  },
+});
